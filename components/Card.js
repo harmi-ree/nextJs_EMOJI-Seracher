d69583b0e2749e68f44419080aa2d8240fa2ef8b
harumi <hrm.goo29@gmail.com>
2023-12-06 15:37:24 +0900

    first commit

--- components/Card.js
+++ components/Card.js
@@ -1,17 +1,17 @@
-import sanitizeHtml from 'sanitize-html';
-import styles from '@/styles/Card.module.css'
-
+import sanitizeHtml from "sanitize-html";
+import styles from "@/styles/Card.module.css";
+/*d*/
 export default function Card(props) {
-  const htmlCode = props.item.htmlCode.join('')
-  const name = props.item.name.replaceAll(/[≊|,].*/g, '')
+  const htmlCode = props.item.htmlCode.join("");
+  const name = props.item.name.replaceAll(/[≊|,].*/g, "");
   return (
     <article className={styles.card}>
       <p
         className={styles.emoji}
-        dangerouslySetInnerHTML={{__html: sanitizeHtml(htmlCode)}}>
-      </p>
-      <p className={styles.name}>{ name }</p>
-      <p className={styles.code}>{ htmlCode}</p>
+        dangerouslySetInnerHTML={{ __html: sanitizeHtml(htmlCode) }}
+      ></p>
+      <p className={styles.name}>{name}</p>
+      <p className={styles.code}>{htmlCode}</p>
     </article>
   );
 }
